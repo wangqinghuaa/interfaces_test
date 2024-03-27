19dd112993f8b3af8299f0661278d86dce64b465
wangqinghuaa <72410012+wangqinghuaa@users.noreply.github.com>
2024-03-27 21:27:34 +0800

    Delete python45期项目 directory

--- python45期项目/ini_env/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# @Time     : 2024-03-17 21:10
-# @Author   : 华
-# @Email    : 867075698@qq.com
-# @File     : __init__.py.py
-# @Software : PyCharm
--- python45期项目/test_case/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# @Time     : 2024-03-15 16:18
-# @Author   : 华
-# @Email    : 867075698@qq.com
-# @File     : __init__.py.py
-# @Software : PyCharm
